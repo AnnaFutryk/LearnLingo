1ea40f0c6df81392ff04300b8dd0bac665e8af7e
AnnaFutryk <anna.futryk@gmail.com>
2023-12-29 18:14:56 +0200

    add navigate to logo

--- src/Logo/Logo.jsx
+++ src/Logo/Logo.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as LogoSvg } from "../images/logo.svg";
 import { LogoTxt, Wrapp } from "./Logo.styled";
 
 const Logo = () => {
+  const navigate = useNavigate();
+
   return (
-    <Wrapp>
+    <Wrapp onClick={() => navigate("/")}>
       <LogoSvg />
       <LogoTxt>LearnLingo</LogoTxt>
     </Wrapp>
