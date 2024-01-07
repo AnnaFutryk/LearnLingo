9460f610076202a789f9cdb2ef9aa0d60cb68b7d
AnnaFutryk <anna.futryk@gmail.com>
2024-01-08 00:50:13 +0200

    Updates

--- static/js/757.7123c188.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklearn_lingo=self.webpackChunklearn_lingo||[]).push([[757],{757:(e,a,l)=>{l.r(a),l.d(a,{default:()=>r});var n=l(184);const r=()=>(0,n.jsx)("p",{children:"favorite page"})}}]);
-//# sourceMappingURL=757.7123c188.chunk.js.map
