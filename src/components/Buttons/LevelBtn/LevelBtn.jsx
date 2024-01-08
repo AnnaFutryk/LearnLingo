63186e6e7f1ecb6e1a821dacea257c2a22ba5700
AnnaFutryk <anna.futryk@gmail.com>
2024-01-09 01:34:54 +0200

    create level btn

---
+++ src/components/Buttons/LevelBtn/LevelBtn.jsx
@@ -0,0 +1,6 @@
+import { LevelButton } from "./LevelBtn.styled";
+
+const LevelBtn = ({ level }) => {
+  return <LevelButton>#{level}</LevelButton>;
+};
+export default LevelBtn;
---
+++ src/components/Buttons/LevelBtn/LevelBtn.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const LevelButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px 12px;
+  border-radius: 35px;
+  background-color: transparent;
+  border: 1px solid rgba(18, 20, 23, 0.2);
+  color: #121417;
+  font-weight: 500;
+  line-height: 1.14;
+`;
