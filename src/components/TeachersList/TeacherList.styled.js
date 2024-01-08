f49a8744f82371b38e62c536a0da571c63ff155d
AnnaFutryk <anna.futryk@gmail.com>
2024-01-09 01:35:58 +0200

    Update TeacherList.styled.js

--- src/components/TeachersList/TeacherList.styled.js
+++ src/components/TeachersList/TeacherList.styled.js
@@ -3,7 +3,11 @@ import styled from "styled-components";
 export const List = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 32px;
   align-items: center;
   justify-content: center;
+`;
+
+export const NoFoundTxt = styled.p`
+  margin-top: 40px;
+  font-size: 36px;
 `;
