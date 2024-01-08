07172a1be16a8fd1ab16c33c6112869572e559e3
AnnaFutryk <anna.futryk@gmail.com>
2024-01-09 01:35:17 +0200

    add lesson form

---
+++ src/components/Forms/BookLessonForm/BookLessonForm.jsx
@@ -0,0 +1,229 @@
+import { Formik } from "formik";
+import * as Yup from "yup";
+import {
+  FormStyled,
+  RadioBtnContainer,
+  RadioBtnTitle,
+  RadioLabel,
+  StyledError,
+  InputField,
+  RadioField,
+  SubmitButtonStyled,
+  InputContainer,
+  InputWrapper,
+} from "./BookLessonForm.styled";
+import { ReactComponent as CheckedSvg } from "../../../images/modal/checked.svg";
+import { ReactComponent as UncheckedSvg } from "../../../images/modal/unchecked.svg";
+
+const initialValues = { name: "", email: "", password: "" };
+const schema = Yup.object().shape({
+  picked: Yup.string()
+    .oneOf(
+      [
+        "careerAndBusiness",
+        "lessonForKids",
+        "livingAbroad",
+        "examsAndCoursework",
+        "cultureTravelHobby",
+      ],
+      "Please choose one option"
+    )
+    .required("Required"),
+  fullName: Yup.string()
+    .required("Name is required")
+    .min(3)
+    .matches(/[A-Za-z]+/, "Name must contain at least 3 letter")
+    .matches(/^[A-Za-z\s]+$/, "Name must contain only letters and spaces"),
+  email: Yup.string()
+    .matches(
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      "Emails: digits, letters, . - _ only, e.g., example@mail.com."
+    )
+    .email("Invalid email format, example@mail.com")
+    .required("Email is required"),
+  phoneNumber: Yup.string().matches(
+    /^\+\d{1,14}$/,
+    "Phone number is not valid"
+  ),
+});
+
+const BookLessonForm = ({ closeModal }) => {
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          initialValues,
+        }}
+        validationSchema={schema}
+        onSubmit={(values, actions) => {
+          console.log(values);
+          actions.resetForm();
+          closeModal();
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <FormStyled onSubmit={handleSubmit}>
+            <div>
+              <RadioBtnTitle>
+                What is your main reason for learning English?
+              </RadioBtnTitle>
+              <RadioBtnContainer
+                role="group"
+                aria-labelledby="radio buttons group"
+              >
+                <RadioLabel>
+                  {values.picked === "careerAndBusiness" ? (
+                    <CheckedSvg />
+                  ) : (
+                    <UncheckedSvg />
+                  )}
+                  <RadioField
+                    type="radio"
+                    name="picked"
+                    value="careerAndBusiness"
+                  />
+                  Career and business
+                </RadioLabel>
+                <RadioLabel>
+                  {values.picked === "lessonForKids" ? (
+                    <CheckedSvg />
+                  ) : (
+                    <UncheckedSvg />
+                  )}
+                  <RadioField
+                    type="radio"
+                    name="picked"
+                    value="lessonForKids"
+                    as={RadioField}
+                  />
+                  Lesson for kids
+                </RadioLabel>
+                <RadioLabel>
+                  {values.picked === "livingAbroad" ? (
+                    <CheckedSvg />
+                  ) : (
+                    <UncheckedSvg />
+                  )}
+                  <RadioField type="radio" name="picked" value="livingAbroad" />
+                  Living abroad
+                </RadioLabel>
+                <RadioLabel>
+                  {values.picked === "examsAndCoursework" ? (
+                    <CheckedSvg />
+                  ) : (
+                    <UncheckedSvg />
+                  )}
+                  <RadioField
+                    type="radio"
+                    name="picked"
+                    value="examsAndCoursework"
+                  />
+                  Exams and coursework
+                </RadioLabel>
+                <RadioLabel>
+                  {values.picked === "cultureTravelHobby" ? (
+                    <CheckedSvg />
+                  ) : (
+                    <UncheckedSvg />
+                  )}
+                  <RadioField
+                    type="radio"
+                    name="picked"
+                    value="cultureTravelHobby"
+                  />
+                  Culture, travel or hobby
+                </RadioLabel>
+                {errors.picked && (
+                  <StyledError className="error">{errors.picked}</StyledError>
+                )}
+              </RadioBtnContainer>
+            </div>
+            <InputContainer>
+              <InputWrapper>
+                <InputField
+                  name="fullName"
+                  placeholder="Full Name"
+                  type="text"
+                  value={values.fullName}
+                  onChange={handleChange}
+                  className={
+                    touched.fullName && !errors.fullName
+                      ? "valid"
+                      : touched.fullName && errors.fullName
+                      ? "invalid"
+                      : ""
+                  }
+                />
+                {touched.fullName && errors.fullName && (
+                  <StyledError className="error">{errors.fullName}</StyledError>
+                )}
+              </InputWrapper>
+              <InputWrapper>
+                <InputField
+                  name="email"
+                  placeholder="Email"
+                  type="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  className={
+                    touched.email && !errors.email
+                      ? "valid"
+                      : touched.email && errors.email
+                      ? "invalid"
+                      : ""
+                  }
+                />
+                {touched.email && errors.email && (
+                  <StyledError className="error">{errors.email}</StyledError>
+                )}
+              </InputWrapper>
+              <InputWrapper>
+                <InputField
+                  name="phoneNumber"
+                  placeholder="Phone number"
+                  type="phone"
+                  value={values.phoneNumber}
+                  onChange={handleChange}
+                  className={
+                    touched.phoneNumber && !errors.phoneNumber
+                      ? "valid"
+                      : touched.phoneNumber && errors.phoneNumber
+                      ? "invalid"
+                      : ""
+                  }
+                />
+                {touched.phoneNumber && errors.phoneNumber && (
+                  <StyledError className="error">
+                    {errors.phoneNumber}
+                  </StyledError>
+                )}
+              </InputWrapper>
+            </InputContainer>
+
+            <SubmitButtonStyled
+              disabled={
+                isSubmitting ||
+                values.picked === "" ||
+                values.fullName === "" ||
+                values.email === "" ||
+                values.phoneNumber === ""
+              }
+              type="submit"
+            >
+              Book
+            </SubmitButtonStyled>
+          </FormStyled>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default BookLessonForm;
---
+++ src/components/Forms/BookLessonForm/BookLessonForm.styled.js
@@ -0,0 +1,89 @@
+import { Field, Form } from "formik";
+import styled from "styled-components";
+
+export const FormStyled = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  margin-top: 40px;
+`;
+export const RadioBtnTitle = styled.h3`
+  margin-bottom: 20px;
+  color: #121417;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.333;
+`;
+export const RadioBtnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`;
+
+export const RadioLabel = styled.label`
+  color: #121417;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.375;
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
+export const RadioField = styled(Field)`
+  display: none;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`;
+
+export const InputWrapper = styled.div`
+  box-sizing: border-box;
+  width: 472px;
+  position: relative;
+`;
+
+export const InputField = styled(Field)`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 18px;
+  border-radius: 12px;
+  border: 1px solid rgba(18, 20, 23, 0.1);
+  outline: none;
+  color: #121417;
+  &.valid {
+    border-color: green;
+  }
+
+  &.invalid {
+    border-color: red;
+  }
+`;
+
+export const StyledError = styled.p`
+  margin-top: 2px;
+  font-size: 12px;
+  font-weight: 400;
+  color: #da1414;
+  margin-left: 24px;
+`;
+
+export const SubmitButtonStyled = styled.button`
+  display: inline-flex;
+  padding: 16px;
+  width: 472px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  border-color: transparent;
+  background-color: #f4c550;
+  color: #121417;
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 1.555;
+  outline: none;
+`;
